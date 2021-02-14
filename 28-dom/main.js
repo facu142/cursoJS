9cf56c0034c89b543e12d78fe701b7fd442dc641
Facundo Montenegro <55951576+facu142@users.noreply.github.com>
2021-02-13 22:20:06 -0300

    agrega 28-dom

--- 28-dom/main.js
+++ 28-dom/main.js
@@ -1,27 +1,38 @@
   
 /* 
 Atributos
-    element.getAttribute('attribute')
-    element.setAttribute('attribute', value)
+    element.getAttribute('attribute')               // Devuelve el atributo
+    element.setAttribute('attribute', value)        // Asigna valor al atrinbuto
 Clases
-    element.classList.add('class','class',...)
-    element.classList.remove('class','class',...)
-    element.classList.contains('class')
-    element.classList.replace('oldClass', newClass)
+    element.classList.add('class','class',...)      // Añadir clases 
+    element.classList.remove('class','class',...)   // Eliminar clases
+    element.classList.contains('class')             // Devuelve true o false si tiene la clase o no 
+    element.classList.replace('oldClass', newClass) // Sustituye una clase por otra
     
-    element.classList.toggle('class'[,force])
+    element.classList.toggle('class'[,force])       // Si tiene la clase se la quita y si no la tiene se la asigna    
 atributos directos
     id
     value
 */
 
 
-const title = document.querySelector('#title'); 
-const nombre = document.querySelector('#name'); 
+const title = document.querySelector('#title');
+const nombre = document.querySelector('#name');
+ 
 
 
-// console.log(name.getAttribute('type'));
-/* 
-nombre.setAttribute('type','number')
-nombre.setAttribute('type','date')
-*/
+nombre.setAttribute('type', 'radio')
+console.log(nombre.getAttribute('type'));
+
+title.classList.add('main-title', 'other-title')
+title.classList.remove('main-title', 'other-title')
+
+
+//console.log(title);
+
+console.log(title.classList.contains('title'));
+
+console.log(title.classList.contains('main-title'));
+
+title.classList.replace('title', 'main-title');
+console.log(title);
