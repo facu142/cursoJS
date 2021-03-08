98c66fb3cf163087976943dbf5d50176de7b31af
facu142 <55951576+facu142@users.noreply.github.com>
2021-03-08 19:17:08 -0300

    actualiza online/offline api

--- 61-api-online-offline/scripts.js
+++ 61-api-online-offline/scripts.js
@@ -1,2 +1,24 @@
-const alert = document.querySelector('#alert');
+const $alert = document.querySelector('#alert');
 
+addEventListener('online', (e) => {
+    setAlert(1);
+})
+
+addEventListener('offline', (e) => {
+    setAlert(0);
+})
+
+const setAlert = (status) => {
+    $alert.classList.remove('alert--online');
+    $alert.classList.remove('alert--offline');
+
+    status === 0 ? 
+    setTimeout(() => {
+        $alert.textContent = 'Vaya, parece que no tienes internet';
+        $alert.classList.add('alert--offline');
+    }, 100) : 
+    setTimeout(() => {
+        $alert.textContent = 'Conexión establecida';
+        $alert.classList.add('alert--online');
+    }, 100); 
+}
