f4374df315e363b1f224d40c9d9b2bb414abdf29
facu142 <55951576+facu142@users.noreply.github.com>
2021-03-08 15:18:40 -0300

    actualiza indexed db II

---
+++ 58-api-indexedDB-actualizar-datos/scripts.js
@@ -0,0 +1,97 @@
+//Especificación oficial: https://developer.mozilla.org/es/docs/IndexedDB-840092-dup
+
+const indexedDB = window.indexedDB;
+const form = document.querySelector('#form');
+const tasks = document.querySelector('#tasks');
+
+if (indexedDB && form) {
+    let db;
+    const request = indexedDB.open('taskList', 1);
+
+    request.onsuccess = (e) => {
+        db = e.target.result;
+        console.log('OPEN', db);
+        readData();
+    }
+    request.onupgradeneeded = (e) => {
+        db = e.target.result;
+        console.log('CREATE', db);
+        const objectStore = db.createObjectStore('tasks', {
+            keyPath: 'taskTitle'
+        })
+    }
+    request.onerror = (err) => {
+        console.log('ERROR', err);
+    }
+
+    const addData = (data) => {
+        // Nos devuelve un objeto de tipo transacción
+
+        const transaction = db.transaction('tasks', 'readwrite');
+
+        // Trabajamos con el método object Store, recibe como parametro el almacen que 
+        // vamos a utilizar
+        const objectStore = transaction.objectStore('tasks');
+        const request = objectStore.add(data);
+        readData();
+    }
+
+    const getData = (key) => {
+        const transaction = db.transaction('tasks' , 'readwrite');
+        const objectStore = transaction.objectStore('tasks');
+        const request = objectStore.get(key);
+        
+        request.onsuccess = () => {
+            form.task.value = request.result.taskTitle;
+            form.priority.value = request.result.taskPriority;
+        }
+    }
+
+
+    const readData = () => {
+        const transaction = db.transaction('tasks', 'readonly');
+        const objectStore = transaction.objectStore('tasks');
+        const request = objectStore.openCursor();
+        const fragment = document.createDocumentFragment();
+
+        request.onsuccess = (e)  => {
+            const cursor = e.target.result;
+            if(cursor){
+                const taskTitle = document.createElement('p')
+                taskTitle.textContent = cursor.value.taskTitle;
+                fragment.appendChild(taskTitle);
+
+                const taskPriority = document.createElement('p')
+                taskPriority.textContent = cursor.value.taskPriority;
+                fragment.appendChild(taskPriority);
+
+                const taskUpdate = document.createElement('button');
+                taskUpdate.dataset.type = 'update';
+                taskUpdate.textContent = 'Update';
+                fragment.appendChild(taskUpdate);
+
+                const taskDelate = document.createElement('button');
+                taskDelate.textContent = 'Delate';
+
+                cursor.continue()
+            }else{
+                tasks.textContent = '';
+                tasks.appendChild(fragment);
+            }
+        }
+
+    }
+
+    form.addEventListener('submit', (e) => {
+
+        e.preventDefault();
+        const data = {
+            taskTitle: e.target.task.value,
+            taskPriority: e.target.priority.value
+        }
+        addData(data)
+
+        console.log(data);
+    })
+
+}
